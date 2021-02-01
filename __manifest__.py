8a33ff2f5a05a912f63b4f2443e50d7631cff3e3
jefferson <jeffer.leo93@gmail.com>
2021-02-01 13:14:58 -0600

    init

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Geducar',
+    'version': '1.0',
+    'category': 'Hidden',
+    'sequence': 6,
+    'summary': 'Modulo Geducar',
+    'description': """
+
+""",
+    'depends': ['sale'],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
