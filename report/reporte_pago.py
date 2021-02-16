e946bea5c61c4083ac8097fe04f9c92a43c2cb16
jeffrepo <jeffer.leo93@gmail.com>
2021-02-16 10:43:10 -0600

    Formato

--- report/reporte_pago.py
+++ report/reporte_pago.py
@@ -109,9 +109,9 @@ def num_a_letras(num, completo=True):
         return num_en_letras
 
     if decimal == 0:
-        letras = '%s exactos' % num_en_letras
+        letras = '%s quetzales' % num_en_letras
     else:
-        letras = '%s con %s/100' % (num_en_letras, decimal)
+        letras = '%s quetzales con %s/100' % (num_en_letras, decimal)
 
     return letras
 
